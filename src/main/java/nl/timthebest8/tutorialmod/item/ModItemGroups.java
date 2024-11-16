75caedbbb4b148e8a872220436155e7f4ad47cde
timthebest8 <timroelands8@gmail.com>
2024-11-16 20:37:52 +0100

    -Added Pink Garnet (Deepslate) Ore

--- src/main/java/nl/timthebest8/tutorialmod/item/ModItemGroups.java
+++ src/main/java/nl/timthebest8/tutorialmod/item/ModItemGroups.java
@@ -11,7 +11,7 @@ import nl.timthebest8.tutorialmod.TutorialMod;
 import nl.timthebest8.tutorialmod.block.ModBlocks;
 
 public class ModItemGroups {
-    public static final ItemGroup PINK_GARNET_ITEMS_GROUPS = Registry.register(Registries.ITEM_GROUP,
+    public static final ItemGroup PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(TutorialMod.MOD_ID, "pink_garnet_items"),
             FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_GARNET))
                     .displayName(Text.translatable("itemgroup.tutorialmod.pink_garnet_items"))
@@ -20,13 +20,16 @@ public class ModItemGroups {
                         entries.add(ModItems.RAW_PINK_GARNET);
                     }).build());
 
-    public static final ItemGroup PINK_GARNET_BLOCKS_GROUPS = Registry.register(Registries.ITEM_GROUP,
+    public static final ItemGroup PINK_GARNET_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(TutorialMod.MOD_ID, "pink_garnet_blocks"),
             FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.PINK_GARNET_BLOCK))
                     .displayName(Text.translatable("itemgroup.tutorialmod.pink_garnet_blocks"))
                     .entries((displayContext, entries) -> {
                         entries.add(ModBlocks.PINK_GARNET_BLOCK);
                         entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+
+                        entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+                        entries.add(ModBlocks.PINK_GARNET_ORE);
                     }).build());
 
     public static void registerItemGroups() {
