96713a768dfc4435d8f0c5309a679ad3128dc368
timthebest8 <timroelands8@gmail.com>
2024-11-16 16:45:28 +0100

    - Added item: Pink Garnet

--- src/main/java/nl/timthebest8/tutorialmod/TutorialMod.java
+++ src/main/java/nl/timthebest8/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package nl.timthebest8.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import nl.timthebest8.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
