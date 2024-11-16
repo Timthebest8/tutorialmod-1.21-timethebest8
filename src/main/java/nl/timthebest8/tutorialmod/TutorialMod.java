728164b00ca08f1b0109c8146004fc99f125f012
timthebest8 <timroelands8@gmail.com>
2024-11-16 17:45:49 +0100

    - Added Block: Block of Pink Garnet

--- src/main/java/nl/timthebest8/tutorialmod/TutorialMod.java
+++ src/main/java/nl/timthebest8/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package nl.timthebest8.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import nl.timthebest8.tutorialmod.block.ModBlocks;
 import nl.timthebest8.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
